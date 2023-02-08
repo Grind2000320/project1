5e963a49d31915858fe553f9f4112ebd3d7a1620
Grind2000320 <2371012199@qq.com>
2023-02-08 21:23:05 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// import { useStore } from "vuex";
+// import { useRouter } from "vue-router";
+// const store = useStore();
+// const router = useRouter();
+// store.commit("addMenu", router);
+</script>
+
+<template>
+  <router-view />
+</template>
+
+<style lang="less">
+#app {
+  height: 100%; 
+}
+</style>
